d8eb5bb9d115b5f8209cadf1bb3bcfc986e100bc
mikwee <dev@mikwee.anonaddy.me>
2026-09-23 16:16:57 +0300

    ♻ Refactored === operator into regex pattern matching

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ export default function fragmentPlugin(md) {
     marker: ":",
 
     validate(params) {
-      return params.trim() === "fragment"
+      return params.trim(),match(/^fragment\s+(.*)$/)
     },
 
     render(tokens, index) {
